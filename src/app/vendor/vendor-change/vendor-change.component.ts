d13225d41b559848b91ffecc2ab6270f94645282
TrevorShepherd <Trevor.R.Shepherd@gmail.com>
2023-04-26 15:34:22 -0400

    Completed the Vendor.

--- src/app/vendor/vendor-change/vendor-change.component.ts
+++ src/app/vendor/vendor-change/vendor-change.component.ts
@@ -1,4 +1,8 @@
+import { ActivatedRoute, Router } from '@angular/router';
 import { Component } from '@angular/core';
+
+import { Vendor } from '../vendor.class';
+import { VendorService } from '../vendor.service';
 
 @Component({
   selector: 'app-vendor-change',
@@ -6,5 +10,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./vendor-change.component.css']
 })
 export class VendorChangeComponent {
+
+  pageName = "Vendor Change"
+  vendor!: Vendor;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private vendorService: VendorService
+  ) { }
+
+  saveVendor(): void {
+    this.vendorService.change(this.vendor).subscribe({
+      next: (response) => {
+        console.debug("User Successfully Changed!", response);
+        this.router.navigateByUrl("/vendor/vendor-list");
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    let id: number = Number(this.activatedRoute.snapshot.params["id"]);
+    this.vendorService.get(id).subscribe({
+      next: (response) => {
+        console.debug("Retrieved Vendor To Change!", response);
+        this.vendor = response;
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  }
 
 }
