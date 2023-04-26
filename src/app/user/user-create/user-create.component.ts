769725abc49ee7369628340168315dfbc038f5b3
TrevorShepherd <Trevor.R.Shepherd@gmail.com>
2023-04-26 11:34:31 -0400

    Completed the user.

--- src/app/user/user-create/user-create.component.ts
+++ src/app/user/user-create/user-create.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { User } from '../user.class';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-user-create',
@@ -6,5 +10,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-create.component.css']
 })
 export class UserCreateComponent {
+
+  pageName = "User Create"
+  newUser: User = new User();
+
+  constructor(
+    private router: Router,
+    private userService: UserService
+  ) { }
+
+  createUser(): void {
+    this.userService.create(this.newUser).subscribe({
+      next: (response) => {
+        console.debug("User Successfully Created!", response);
+        this.router.navigateByUrl("/user/user-list");
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    this.newUser.id = 0;
+  }
 
 }
